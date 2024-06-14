0446e42507c13917e8871d341e9b94a223b4f723
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-14 10:44:59 +0200

    Select

--- app-angular/src/app/shared/shared.module.ts
+++ app-angular/src/app/shared/shared.module.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
   ],
-  declarations: [],
+  declarations: [
+    SelectComponent
+  ],
   exports: [
     CommonModule,
     FormsModule,
+    SelectComponent,
+
   ]
 })
 export class SharedModule { }
