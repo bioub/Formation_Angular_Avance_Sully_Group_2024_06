e1057c0b4c7d94e4b646acd43c1331e9806c359d
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-14 11:52:53 +0200

    Exercice Input / Output

--- app-angular/README.md
+++ app-angular/README.md
@@ -7,3 +7,10 @@ Editer la config des modules de test dans :
 `src/app/todos/todo-form/todo-form.component.spec.ts` qui dépend de la directive `ngModel` (exportée par `FormsModule`)
 
 `src/app/todos/todos/todos.component.spec.ts` qui dépend de `TodoFormComponent` et `TodosListComponent`. Attention `TodoFormComponent` dépend lui même de `FormsModule` (comme dans l'exercice précédent). `TodosListComponent` dépend de `CommonModule` (à cause de `*ngFor`) et `TodoItemComponent`
+
+## Test Unitaires de Composants
+
+Dans `src/app/todos/todo-item/todo-item.component.spec.ts` vérifier que quand on passe un input à `TodoItem` il s'affiche bien dans le composant.
+
+Dans `src/app/todos/todo-form/todo-form.component.spec.ts` vérifier que au submit du formulaire, l'output `onAddTodo` emet bien la valeur contenue dans la propriété `todo`.
+
--- app-angular/src/app/shared/select/select.component.spec.ts
+++ app-angular/src/app/shared/select/select.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectComponent } from './select.component';
+import { $ } from 'protractor';
 
 describe('SelectComponent', () => {
   let component: SelectComponent;
@@ -14,10 +15,38 @@ describe('SelectComponent', () => {
 
     fixture = TestBed.createComponent(SelectComponent);
     component = fixture.componentInstance;
+    fixture.autoDetectChanges();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display inputs', () => {
+    component.items = ['Jean', 'Pierre', 'Paul'];
+    component.selected = 'Toto';
+    fixture.detectChanges();
+    expect(fixture.nativeElement.textContent).toContain('Toto');
+
+    fixture.nativeElement.querySelector('.selected').dispatchEvent(new MouseEvent('click'));
+
+    expect(fixture.nativeElement.textContent).toContain('Jean');
+    expect(fixture.nativeElement.textContent).toContain('Pierre');
+    expect(fixture.nativeElement.textContent).toContain('Paul');
+  });
+
+  it('should emit ouputs', () => {
+    component.items = ['Jean', 'Pierre', 'Paul'];
+    component.selected = 'Toto';
+    fixture.detectChanges();
+    fixture.nativeElement.querySelector('.selected').dispatchEvent(new MouseEvent('click'));
+
+    const spy = jasmine.createSpy();
+    component.selectedChange.subscribe(spy)
+
+    fixture.nativeElement.querySelector('.item:last-child').dispatchEvent(new MouseEvent('click'));
+
+    expect(spy).toHaveBeenCalledOnceWith('Paul');
   });
 });
